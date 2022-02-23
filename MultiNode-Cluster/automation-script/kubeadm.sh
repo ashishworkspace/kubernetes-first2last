fa0d93d941afb82bd0b2427bf7d2bbaedf54ddd2
ashish <get.mail.ashish@gmail.com>
2022-02-23 21:11:50 +0530

    Master & Worker Node Setup on REDHAT

---
+++ MultiNode-Cluster/automation-script/kubeadm.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Installing kubeadm, kublet & kubectl 
+
+swapoff -a
+
+yum install docker iproute-tc -y
+systemclt start docker 
+
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+
+# Set SELinux in permissive mode (effectively disabling it)
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+sudo systemctl enable --now kubelet
+
+
+
+# docker by default use cgroupfs driver but k8s need docker to run with driver systemd
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"]
+}
+EOF
+
+systemctl restart docker
+systemctl enable docker --now
+
+
+
+
